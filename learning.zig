47d72c6e5e79fefc0870025261f9a073e6c660e1
soerenlemke <61249756+soerenlemke@users.noreply.github.com>
2025-11-12 20:50:12 +0100

    language overview part 1 done

--- learning.zig
+++ learning.zig
@@ -1,9 +1,5 @@
 const std = @import("std");
 
 pub fn main() void {
-    var a = [_]i32{ 1, 2, 3, 4, 5 };
-    var end: usize = 3;
-    end += 1;
-    var b = a[1..end];
-    b = b[1..];
+    std.debug.print("{}\n", .{.{ .year = 2023, .month = 8 }});
 }
