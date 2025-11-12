0f56ab9da00e72bb7f5f6570bd3ef29c114e1cf4
soerenlemke <61249756+soerenlemke@users.noreply.github.com>
2025-11-12 20:19:54 +0100

    arrays and slices done & gitignore added

--- learning.zig
+++ learning.zig
@@ -1,46 +1,9 @@
-//! first program from openmymind learning zig
-
 const std = @import("std");
 
-const SUPER_POWER = 9000;
-
-/// pub fn main needed for not getting compiler error
 pub fn main() void {
-    const user: User = .{
-        .power = 9001,
-        .name = "Goku",
-    };
-
-    std.debug.print("{s}`s power is {d}\n", .{ user.name, user.power });
-
-    user.diagnose();
-
-    const a = 8999;
-    const b = 2;
-    const sum = add(a, b);
-    std.debug.print("{d} + {d} = {d}\n", .{ a, b, sum });
-}
-
-/// User struct
-const User = struct {
-    power: u64,
-    name: []const u8,
-
-    pub fn init(name: []const u8, power: u64) User {
-        return .{
-            .name = name,
-            .power = power,
-        };
-    }
-
-    pub fn diagnose(user: User) void {
-        if (user.power >= SUPER_POWER) {
-            std.debug.print("it`s over {d}!!!\n", .{SUPER_POWER});
-        }
-    }
-};
-
-/// simple add function
-fn add(a: i64, b: i64) i64 {
-    return a + b;
+    var a = [_]i32{ 1, 2, 3, 4, 5 };
+    var end: usize = 3;
+    end += 1;
+    var b = a[1..end];
+    b = b[1..];
 }
