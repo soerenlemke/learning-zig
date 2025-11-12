0d342c7c3c7440f584e60ece4709e4d36f40cde5
soerenlemke <61249756+soerenlemke@users.noreply.github.com>
2025-11-12 21:59:00 +0100

    stack memory done

--- learning.zig
+++ learning.zig
@@ -1,23 +1,22 @@
 const std = @import("std");
 
 pub fn main() void {
-    const leto = User{
-        .id = 1,
-        .power = 9001,
-        .manager = null,
-    };
+    const user1 = User.init(1, 10);
+    const user2 = User.init(2, 20);
 
-    const duncan = User{
-        .id = 1,
-        .power = 9001,
-        .manager = &leto,
-    };
-
-    std.debug.print("{any}\n{any}", .{ leto, duncan });
+    std.debug.print("User {d} has power of {d}\n", .{ user1.id, user1.power });
+    std.debug.print("User {d} has power of {d}\n", .{ user2.id, user2.power });
 }
 
 pub const User = struct {
     id: u64,
     power: i32,
-    manager: ?*const User,
+
+    fn init(id: u64, power: i32) *User {
+        var user = User{
+            .id = id,
+            .power = power,
+        };
+        return &user;
+    }
 };
