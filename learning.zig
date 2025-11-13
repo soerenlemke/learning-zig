7c33fc37094431bad58e47058c69a9521e47bdc8
soerenlemke <61249756+soerenlemke@users.noreply.github.com>
2025-11-13 22:26:52 +0100

    heap & memory allocators done

--- learning.zig
+++ learning.zig
@@ -1,22 +1,83 @@
 const std = @import("std");
+const builtin = @import("builtin");
+const Allocator = std.mem.Allocator;
 
-pub fn main() void {
-    const user1 = User.init(1, 10);
-    const user2 = User.init(2, 20);
+pub fn main() !void {
+    var gpa: std.heap.GeneralPurposeAllocator(.{}) = .empty;
+    const allocator = if (builtin.mode == .Debug) gpa.allocator() else std.heap.c_allocator;
 
-    std.debug.print("User {d} has power of {d}\n", .{ user1.id, user1.power });
-    std.debug.print("User {d} has power of {d}\n", .{ user2.id, user2.power });
+    defer if (builtin.mode == .Debug) {
+        if (gpa.detectLeaks()) {
+            std.posix.exit(1);
+        }
+    };
+
+    var arena = std.heap.ArenaAllocator.init(allocator);
+    defer arena.deinit();
+    const aa = arena.allocator();
+
+    var list = try IntList.init(aa);
+    defer list.deinit();
+
+    for (0..10) |i| {
+        try list.add(@intCast(i));
+    }
+
+    std.debug.print("{any}\n", .{list.items[0..list.pos]});
 }
 
-pub const User = struct {
-    id: u64,
-    power: i32,
+pub const IntList = struct {
+    pos: usize,
+    items: []i64,
+    allocator: Allocator,
 
-    fn init(id: u64, power: i32) *User {
-        var user = User{
-            .id = id,
-            .power = power,
+    fn init(allocator: Allocator) !IntList {
+        return .{
+            .pos = 0,
+            .allocator = allocator,
+            .items = try allocator.alloc(i64, 4),
         };
-        return &user;
+    }
+
+    fn deinit(self: IntList) void {
+        self.allocator.free(self.items);
+    }
+
+    fn add(self: *IntList, value: i64) !void {
+        const pos = self.pos;
+        const len = self.items.len;
+
+        if (pos == len) {
+            // we've run out of space
+            // create a new slice that's twice as large
+            var larger = try self.allocator.alloc(i64, len * 2);
+
+            // copy the items we previously added to our new space
+            @memcpy(larger[0..len], self.items);
+            self.allocator.free(self.items);
+
+            self.items = larger;
+        }
+
+        self.items[pos] = value;
+        self.pos = pos + 1;
     }
 };
+
+const testing = std.testing;
+test "IntList: add" {
+    // We're using testing.allocator here!
+    var list = try IntList.init(testing.allocator);
+    defer list.deinit();
+
+    for (0..5) |i| {
+        try list.add(@intCast(i + 10));
+    }
+
+    try testing.expectEqual(@as(usize, 5), list.pos);
+    try testing.expectEqual(@as(i64, 10), list.items[0]);
+    try testing.expectEqual(@as(i64, 11), list.items[1]);
+    try testing.expectEqual(@as(i64, 12), list.items[2]);
+    try testing.expectEqual(@as(i64, 13), list.items[3]);
+    try testing.expectEqual(@as(i64, 14), list.items[4]);
+}
