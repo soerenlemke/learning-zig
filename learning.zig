0bf141c12437d9d0f9298ccce579504c378d88ff
soerenlemke <61249756+soerenlemke@users.noreply.github.com>
2025-11-14 17:33:37 +0100

    generics done

--- learning.zig
+++ learning.zig
@@ -1,22 +1,11 @@
 const std = @import("std");
-const builtin = @import("builtin");
 const Allocator = std.mem.Allocator;
 
 pub fn main() !void {
-    var gpa: std.heap.GeneralPurposeAllocator(.{}) = .empty;
-    const allocator = if (builtin.mode == .Debug) gpa.allocator() else std.heap.c_allocator;
+    var gpa = std.heap.DebugAllocator(.{}){};
+    const allocator = gpa.allocator();
 
-    defer if (builtin.mode == .Debug) {
-        if (gpa.detectLeaks()) {
-            std.posix.exit(1);
-        }
-    };
-
-    var arena = std.heap.ArenaAllocator.init(allocator);
-    defer arena.deinit();
-    const aa = arena.allocator();
-
-    var list = try IntList.init(aa);
+    var list = try List(u32).init(allocator);
     defer list.deinit();
 
     for (0..10) |i| {
@@ -26,58 +15,45 @@ pub fn main() !void {
     std.debug.print("{any}\n", .{list.items[0..list.pos]});
 }
 
-pub const IntList = struct {
-    pos: usize,
-    items: []i64,
-    allocator: Allocator,
+fn List(comptime T: type) type {
+    return struct {
+        pos: usize,
+        items: []T,
+        allocator: Allocator,
 
-    fn init(allocator: Allocator) !IntList {
-        return .{
-            .pos = 0,
-            .allocator = allocator,
-            .items = try allocator.alloc(i64, 4),
-        };
-    }
+        const Self = @This();
 
-    fn deinit(self: IntList) void {
-        self.allocator.free(self.items);
-    }
-
-    fn add(self: *IntList, value: i64) !void {
-        const pos = self.pos;
-        const len = self.items.len;
-
-        if (pos == len) {
-            // we've run out of space
-            // create a new slice that's twice as large
-            var larger = try self.allocator.alloc(i64, len * 2);
-
-            // copy the items we previously added to our new space
-            @memcpy(larger[0..len], self.items);
-            self.allocator.free(self.items);
-
-            self.items = larger;
+        fn init(allocator: Allocator) !Self {
+            return .{
+                .pos = 0,
+                .allocator = allocator,
+                .items = try allocator.alloc(T, 4),
+            };
         }
 
-        self.items[pos] = value;
-        self.pos = pos + 1;
-    }
-};
+        fn deinit(self: Self) void {
+            self.allocator.free(self.items);
+        }
 
-const testing = std.testing;
-test "IntList: add" {
-    // We're using testing.allocator here!
-    var list = try IntList.init(testing.allocator);
-    defer list.deinit();
+        fn add(self: *Self, value: T) !void {
+            const pos = self.pos;
+            const len = self.items.len;
 
-    for (0..5) |i| {
-        try list.add(@intCast(i + 10));
-    }
+            if (pos == len) {
+                // we've run out of space
+                // create a new slice that's twice as large
+                var larger = try self.allocator.alloc(T, len * 2);
 
-    try testing.expectEqual(@as(usize, 5), list.pos);
-    try testing.expectEqual(@as(i64, 10), list.items[0]);
-    try testing.expectEqual(@as(i64, 11), list.items[1]);
-    try testing.expectEqual(@as(i64, 12), list.items[2]);
-    try testing.expectEqual(@as(i64, 13), list.items[3]);
-    try testing.expectEqual(@as(i64, 14), list.items[4]);
+                // copy the items we previously added to our new space
+                @memcpy(larger[0..len], self.items);
+
+                self.allocator.free(self.items);
+
+                self.items = larger;
+            }
+
+            self.items[pos] = value;
+            self.pos = pos + 1;
+        }
+    };
 }
